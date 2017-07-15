9939819eddf28b1e004ae9b913970b3e343cde75
siddharth719 <siddharthhari06@gmail.com>
2017-07-15 16:09:13 +0530

    Create vowel.py

---
+++ vowel.py
@@ -0,0 +1,11 @@
+string1 = "test"
+string2 = "test"
+if string1 == string2:
+    print 'they are the same'
+
+char1=str(input("Enter a word"))
+if (char1=="a"): 
+	#or "e" or "i" or "o" or "u":
+   print "vowel"
+else:
+	print "consanant"
