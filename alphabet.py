782b18ceb0adcef0e7ea2e94e84c17585f54b54c
siddharth719 <siddharthhari06@gmail.com>
2017-07-15 16:34:28 +0530

    Create alphabet.py

---
+++ alphabet.py
@@ -0,0 +1,12 @@
+char=str(input("enter a alphabet"))
+print ("String entered is ",char)
+print("Enter 0 for exit.")
+char = input("Enter any character: ")
+if char == '0':
+	print ("Exiting")
+'''else:
+	if((char>='a' and char<='z') or (char>='A' and char<='Z')):
+		print(char, "is an alphabet.\n")
+	else:
+		print(char, "is not an alphabet.\n")
+'''
